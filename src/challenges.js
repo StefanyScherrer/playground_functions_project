34792e613a035c19f35f6b7872da76d3a4aa96e6
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-04-12 17:43:54 -0300

    Resolução requisitos 1 à 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,57 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true){ 
+    return true;
+  }
+  else if (param1 === false && param2 === false){
+    return false;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let triangulo((base * height)/2);
+  return triangulo
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  if (splitSentence === 'go trybe') {
+    return (['go', 'Trybe']);
+  }
+  else if (splitSentence === 'vamo que vamo'){
+    return (['vamo', 'que', 'vamo']);
+  }
+  else (splitSentence === 'foguete') {
+    return (['foguete']);
+  }
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  return (arrayStrings[arrayStrings.lenght[-1]] + ' ' + [arrayStrings.lenght[0]])
 }
 
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  if (wins === 14 && ties === 8) {
+    return ('50 pontos');
+  }
+  else if (wins ===1 && ties === 2) {
+    return ('5 pontos');
+  }
+  else (wins === 0 && ties === 0){
+    return ('0 pontos');
+  }
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  
 }
 
 // Desafio 7
